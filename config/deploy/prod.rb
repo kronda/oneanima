ac78fb94752bbfab5c7db39a6beb8cd1da6da4d1
Kronda <ephany@gmail.com>
2014-07-19 19:30:23 -0700

    config files

--- config/deploy/prod.rb
+++ config/deploy/prod.rb
@@ -1,18 +1,18 @@
 # Set the deployment directory on the target hosts.
-set :deploy_to, "/home/krondaco/sites/#{application}.com"
+set :deploy_to, "/home/oneanima/sites/#{application}.com"
 
 # Use the correct branch on github. Uncomment this if you have set up seperate branches for each staging area
 set :branch, "prod"
 
 # The hostnames to deploy to.
-role :web, "kronda.com"
+role :web, "173.192.191.35"
 
 # Specify one of the web servers to use for database backups or updates.
 # This server should also be running Wordpress.
-role :db, "kronda.com", :primary => true
+role :db, "173.192.191.35", :primary => true
 
 # The path to wp-cli
 set :wp, "cd #{current_path}/#{app_root} ; /usr/local/bin/wp"
 
 # The username on the target system, if different from your local username
-ssh_options[:user] = 'krondaco'
+ssh_options[:user] = 'oneanima'
